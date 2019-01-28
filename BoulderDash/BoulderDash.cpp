64e13750fda644a01bc6391edcd01adce881b5d3
Dominik Wolny <wolny.dominik@outlook.com>
2019-01-28 14:06:28 +0100

    Added few maps, added basic stone movement, fixed rendering bug (wrong textures were written as map.

--- BoulderDash/BoulderDash.cpp
+++ BoulderDash/BoulderDash.cpp
@@ -6,6 +6,8 @@
 #include <fstream>
 #include <string>
 #include <SFML/Graphics.hpp>
+#include <thread>
+#include <Windows.h>
 
 void readLevel(char **map, int lvl) {
 	std::string name = "maps/map" + std::to_string(lvl) + ".txt";
@@ -18,8 +20,32 @@ void readLevel(char **map, int lvl) {
 }
 
 struct player {
-	unsigned short int x, y, points = 0, p_rem = 0, _x, _y, hp;
+	unsigned short int x, y, points = 0, p_rem = 0, _x, _y, hp, finished = 0; //finished - 0: no, 1: yes
 };
+
+void moveStoneTh(char **map, player P) {
+	while (P.finished == 0) {
+		Sleep(1000);
+		for (int i = 0; i < 16; i++) {
+			for (int j = 1; j < 102; j++) {
+				if (map[i][j] == 'S') {
+					if (map[i + 1][j] == 'A') {
+						map[i][j] = 'A';
+						map[i + 1][j] = 'S';
+					}
+					else if (map[i + 1][j + 1] == 'A') {
+						map[i][j] = 'A';
+						map[i][j + 1] = 'S';
+					}
+					else if (map[i + 1][j - 1] == 'A') {
+						map[i][j] = 'A';
+						map[i][j - 1] = 'S';
+					}
+				}
+			}
+		}
+	}
+}
 
 void drawMap(char **map, sf::Sprite *sprites, sf::Sprite **bar, sf::RenderTarget &window, int off_x, int off_y, player P) {
 	for (int i = 0; i < 17; i++) {
@@ -74,9 +100,22 @@ int main()
 		std::cout << "Texture load fail\n";
 	else
 		std::cout << "Texture load successful!\n";
+	
+	sf::Sprite *sprites = new sf::Sprite[7];
+
+	for (int i = 0; i < 6; i++)
+		sprites[i].setTexture(texture);
+
+	sprites[0].setTextureRect(sf::IntRect(0, 0, 64, 64));
+	sprites[1].setTextureRect(sf::IntRect(64, 0, 64, 64));
+	sprites[2].setTextureRect(sf::IntRect(128, 0, 64, 64));
+	sprites[3].setTextureRect(sf::IntRect(192, 0, 64, 64));
+	sprites[4].setTextureRect(sf::IntRect(64, 64, 64, 64));
+	sprites[5].setTextureRect(sf::IntRect(128, 64, 64, 64));
+	sprites[6].setTextureRect(sf::IntRect(192, 64, 64, 64));
 
 	sf::Texture text;
-	if (!texture.loadFromFile("text.png"))
+	if (!text.loadFromFile("text.png"))
 		std::cout << "Texture load fail\n";
 	else
 		std::cout << "Texture load successful!\n";
@@ -106,8 +145,7 @@ int main()
 	else
 		bar[0][1].setTextureRect(sf::IntRect((P.p_rem + P.p_rem * 3), 0, 3, 5));
 
-	for(int i=0;i<P.hp;i++)
-		bar[1][i].setTextureRect(sf::IntRect(42, 0, 7, 6));
+	bar[1][0].setTextureRect(sf::IntRect(42, 0, 7, 6));
 
 	bar[2][3].setTextureRect(sf::IntRect(((level % 10) * 3) + (level % 10), 0, 3, 5));
 	bar[2][2].setTextureRect(sf::IntRect((((level / 10) % 10) * 3) + ((level / 10) % 10), 0, 3, 5));
@@ -115,27 +153,10 @@ int main()
 
 	bar[3][2].setTextureRect(sf::IntRect(((level % 10) * 3) + (level % 10), 0, 3, 5));
 	bar[3][1].setTextureRect(sf::IntRect((((level/10) % 10) * 3) + ((level / 10) % 10), 0, 3, 5));
-	
-	sf::Sprite *sprites = new sf::Sprite[7];
-
-	for (int i = 0; i < 6; i++)
-		sprites[i].setTexture(texture);
-
-	sprites[0].setTextureRect(sf::IntRect(0, 0, 64, 64));
-	sprites[1].setTextureRect(sf::IntRect(64, 0, 64, 64));
-	sprites[2].setTextureRect(sf::IntRect(128, 0, 64, 64));
-	sprites[3].setTextureRect(sf::IntRect(192, 0, 64, 64));
-	sprites[4].setTextureRect(sf::IntRect(64, 64, 64, 64));
-	sprites[5].setTextureRect(sf::IntRect(128, 64, 64, 64));
-	sprites[6].setTextureRect(sf::IntRect(192, 64, 64, 64));
-
-
 
 	char** map = new char*[17];
 	for (int i = 0; i < 17; i++)
 		map[i] = new char[103];
-
-	
 
 	readLevel(map, level);
 
@@ -154,6 +175,8 @@ int main()
 			std::cout << "\n";
 	}
 
+	std::thread t1(moveStoneTh, map, P);
+
 	while (window.isOpen())
 	{
 		window.clear();
@@ -166,7 +189,28 @@ int main()
 			if (event.type == sf::Event::KeyPressed)
 			{
 				if (event.key.code == sf::Keyboard::W) {
-					if (map[P.y - 1][P.x] != 'B' && map[P.y - 1][P.x] != 'S' && map[P.y - 1][P.x] != 'E') {
+					if (P.p_rem == P.points && map[P.y - 1][P.x] == 'E') {
+						level++;
+						readLevel(map, level);
+						P.finished = 1;
+						for (int i = 0; i < 17; i++) {
+							for (int j = 0; j < 103; j++) {
+								std::cout << map[i][j] << "";
+								if (map[i][j] == 'X') {
+									P.x = j;
+									P.y = i;
+									P._x = j;
+									P._y = i;
+								}
+								else if (map[i][j] == 'P')
+									P.points++;
+							}
+							std::cout << "\n";
+							off_y = window.getSize().y / 180;
+							off_x = window.getSize().x / 180;
+						}
+					}
+					else if (map[P.y - 1][P.x] != 'B' && map[P.y - 1][P.x] != 'S' && map[P.y - 1][P.x] != 'E') {
 						if (map[P.y - 1][P.x] == 'P')
 							P.points++;
 						map[P.y][P.x] = 'A';
@@ -176,7 +220,28 @@ int main()
 					}
 				}
 				else if (event.key.code == sf::Keyboard::S) {
-					if (map[P.y + 1][P.x] != 'B' && map[P.y + 1][P.x] != 'S' && map[P.y + 1][P.x] != 'E') {
+					if (P.p_rem == P.points && map[P.y + 1][P.x] == 'E') {
+						level++;
+						readLevel(map, level);
+						P.finished = 1;
+						for (int i = 0; i < 17; i++) {
+							for (int j = 0; j < 103; j++) {
+								std::cout << map[i][j] << "";
+								if (map[i][j] == 'X') {
+									P.x = j;
+									P.y = i;
+									P._x = j;
+									P._y = i;
+								}
+								else if (map[i][j] == 'P')
+									P.points++;
+							}
+							std::cout << "\n";
+						}
+						off_y = window.getSize().y / 180;
+						off_x = window.getSize().x / 180;
+					}
+					else if (map[P.y + 1][P.x] != 'B' && map[P.y + 1][P.x] != 'S' && map[P.y + 1][P.x] != 'E') {
 						if (map[P.y + 1][P.x] == 'P')
 							P.points++;
 						map[P.y][P.x] = 'A';
@@ -186,7 +251,28 @@ int main()
 					}
 				}
 				else if (event.key.code == sf::Keyboard::A) {
-					if (map[P.y][P.x - 1] != 'B' && map[P.y][P.x - 1] != 'S' && map[P.y][P.x - 1] != 'E') {
+					if (P.p_rem == P.points && map[P.y][P.x - 1] == 'E') {
+						level++;
+						readLevel(map, level);
+						P.finished = 1;
+						for (int i = 0; i < 17; i++) {
+							for (int j = 0; j < 103; j++) {
+								std::cout << map[i][j] << "";
+								if (map[i][j] == 'X') {
+									P.x = j;
+									P.y = i;
+									P._x = j;
+									P._y = i;
+								}
+								else if (map[i][j] == 'P')
+									P.points++;
+							}
+							std::cout << "\n";
+						}
+						off_y = window.getSize().y / 180;
+						off_x = window.getSize().x / 180;
+					}
+					else if (map[P.y][P.x - 1] != 'B' && map[P.y][P.x - 1] != 'S' && map[P.y][P.x - 1] != 'E') {
 						if (map[P.y][P.x - 1] == 'P')
 							P.points++;
 						map[P.y][P.x] = 'A';
@@ -196,7 +282,29 @@ int main()
 					}
 				}
 				else if (event.key.code == sf::Keyboard::D) {
-					if (map[P.y][P.x + 1] != 'B' && map[P.y][P.x + 1] != 'S' && map[P.y][P.x + 1] != 'E') {
+					if (P.p_rem == P.points && map[P.y][P.x + 1] == 'E') {
+						level++;
+						readLevel(map, level);
+						P.finished = 1;
+						for (int i = 0; i < 17; i++) {
+							for (int j = 0; j < 103; j++) {
+								std::cout << map[i][j] << "";
+								if (map[i][j] == 'X') {
+									P.x = j;
+									P.y = i;
+									P._x = j;
+									P._y = i;
+								}
+								else if (map[i][j] == 'P')
+									P.points++;
+							}
+							std::cout << "\n";
+						}
+						off_y = window.getSize().y / 180;
+						off_x = window.getSize().x / 180;
+
+					}
+					else if (map[P.y][P.x + 1] != 'B' && map[P.y][P.x + 1] != 'S' && map[P.y][P.x + 1] != 'E') {
 						if (map[P.y][P.x + 1] == 'P')
 							P.points++;
 						map[P.y][P.x] = 'A';
@@ -209,43 +317,12 @@ int main()
 			}
 		}
 
-		/*for (int i = 0; i < 17; i++) {
-			for (int j = 0; j < 103; j++) {
-				if (map[i][j] == 'B') {
-					sprites[0].setPosition(sf::Vector2f(j*64.f, i*64.f));
-					window.draw(sprites[0]);
-				}
-				else if (map[i][j] == 'S') {
-					sprites[2].setPosition(sf::Vector2f(j*64.f, i*64.f));
-					window.draw(sprites[2]);
-				}
-				else if (map[i][j] == 'L') {
-					sprites[6].setPosition(sf::Vector2f(j*64.f, i*64.f));
-					window.draw(sprites[6]);
-				}
-				else if (map[i][j] == 'X') {
-					sprites[5].setPosition(sf::Vector2f(j*64.f, i*64.f));
-					window.draw(sprites[5]);
-				}
-				else if (map[i][j] == 'E') {
-					sprites[4].setPosition(sf::Vector2f(j*64.f, i*64.f));
-					window.draw(sprites[4]);
-				}
-				else if (map[i][j] == 'D') {
-					sprites[1].setPosition(sf::Vector2f(j*64.f, i*64.f));
-					window.draw(sprites[1]);
-				}
-				else if (map[i][j] == 'P') {
-					sprites[3].setPosition(sf::Vector2f(j*64.f, i*64.f));
-					window.draw(sprites[3]);
-				}
-			}
-		}*/
-
 		window.display();
 
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Q))
 		{
+			P.finished = 1;
+			t1.join();
 			window.close();
 		}
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::R))
